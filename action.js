27ac3a7075909294a1e2999faf4fba91c6028ebb
David <zwssd1980@163.com>
2018-09-20 22:36:17 +0800

    可以弹出了，但会报错，popup一退出，就报错。需要修改。
    TypeError: can't access dead object

--- action.js
+++ action.js
@@ -9,21 +9,62 @@ var createActionsMenuItems = function () {
     var items = [];
     var action = document.getElementById("action");
 
-    items.push({ name: '没有运行中的任务', image: '<span class="fa fa-info"></span>'});
-    items.push({ name: '添加任务', image: '<span class="fa fa-plus-square"></span>'});
+    items.push({ name: '<span class="fa fa-info"></span>没有运行中的任务'});
+    items.push({ name: '<div id="button" class="add-tasks"><span class="fa fa-plus-square"></span>添加任务</div>'});
     items.push({ name: 'HR', image: ''});
-    items.push({ name: '欢迎捐助', image: '<span class="fa fa-heart"></span>'});
+    items.push({ name: '<div id="button" class="welcome-donations"><span class="fa fa-heart"></span>欢迎捐助</div>'});
     items.push({ name: 'HR', image: ''});
-    items.push({ name: '管理面板', image: '<span class="fa fa-cog blue"></span>'});
-    items.push({ name: '帮助|更新日志', image: '<span class="fa fa-info-circle blue"></span>'});
+    items.push({ name: '<div id="button" class="management-panel"><span class="fa fa-cog blue"></span>管理面板</div>'});
+    items.push({ name: '<div id="button" class="help-update-log"><span class="fa fa-info-circle blue"></span>帮助&nbsp;|&nbsp;更新日志</div>'});
     for(var i=0;i<items.length;i++){
         if(items[i].name=='HR'){
             actionHtml += '<hr>';
         }else{
-            actionHtml += '<tr><td>'+items[i].image+'&nbsp;'+items[i].name+'</td></tr><br>';
+            actionHtml += '<tr><td>'+items[i].name+'</td></tr><br>';
         }
     }
     action.innerHTML = actionHtml;
 }
 
+
+const hidePage = `body > :not(.beastify-image) {
+                    display: none;
+                  }`;
+
+function listenForClicks() {
+    document.addEventListener("click", (e) => {
+
+        /**
+         * Given the name of a beast, get the URL to the corresponding image.
+         */
+        function beastNameToURL(beastName) {
+            alert(beastName);
+            switch (beastName) {
+                case "add-tasks":
+                    alert('button');
+                    return browser.extension.getURL("images/info.png");
+                case "welcome-donations":
+                    return browser.extension.getURL("images/info.png");
+                case "management-panel":
+                    return browser.extension.getURL("images/info.png");
+                case "help-update-log":
+                    return browser.extension.getURL("images/info.png");
+            }
+        }
+
+
+        /**
+         * Get the active tab,
+         * then call "add-tasks" or "welcome-donations" or "management-panel" or "help-update-log" as appropriate.
+         */
+        if (e.target.id == "button") {
+            alert('a');
+            let url = beastNameToURL(e.target.className);
+            alert('ab');
+            browser.tabs.create({url: url});
+        }
+    });
+}
+
 createActionsMenuItems();
+listenForClicks();
